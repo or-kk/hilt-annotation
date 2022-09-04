d6903f1354b705f98c6bad02ebff42164d36c412
“ralph-12” <tmdrl1026@naver.com>
2022-09-04 15:24:15 +0900

    add HiltViewModel example

---
+++ app/src/main/java/com/example/hilt_annotations/MainViewModel.kt
@@ -0,0 +1,12 @@
+package com.example.hilt_annotations
+
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class MainViewModel @Inject constructor(
+    private val adapter: MainAdapter,
+    private val state: SavedStateHandle
+) : ViewModel() {}

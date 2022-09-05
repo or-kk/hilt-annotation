ca07e3f4d93f5817fb7bff188a872fde9a7147fe
“ralph-12” <tmdrl1026@naver.com>
2022-09-05 20:47:12 +0900

    add application context exmaple

--- app/src/main/java/com/example/hilt_annotations/AnalyticsAdapter.kt
+++ app/src/main/java/com/example/hilt_annotations/AnalyticsAdapter.kt
@@ -1,9 +1,12 @@
 package com.example.hilt_annotations
 
+import android.content.Context
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.android.scopes.ActivityScoped
 import javax.inject.Inject
 
 @ActivityScoped
 class AnalyticsAdapter @Inject constructor(
+    @ApplicationContext val context: Context,
     private val mainService: MainService
 ) { }
